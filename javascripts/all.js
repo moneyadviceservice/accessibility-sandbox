445de805cddff62ac9bbfbdc2669d97c397549b0
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-05-22 15:26:41 +0100

    Site updated to 24b6495

--- javascripts/all.js
+++ javascripts/all.js
@@ -367,3 +367,81 @@
     };
 
 }));
+;(function(window){
+  'use strict';
+
+  var $component;
+  var $componentLabels;
+
+  function setup() {
+    $component = $('.js-component');
+    $componentLabels = $component.find('.component__label');
+
+    $componentLabels.on('click keydown', function(e) {
+      if(e.which === 13 || e.which === 1 || e.which === 32) {
+        handleComponentAction(e, $(this));
+      }
+    });
+  }
+
+  function handleComponentAction(e, $label) {
+    toggleTab($label);
+    setARIAExpanded($componentLabels);
+  }
+
+  function toggleTab($label) {
+    var $content = $label.next('.component__content');
+    if( $label.attr('aria-selected') === 'true'){
+      closeTab($label, $content);
+    }
+    else{
+      openTab($label, $content);
+    }
+  }
+
+  function setARIAExpanded($labels) {
+    $component.attr('aria-expanded', ($componentLabels.filter('[aria-selected="true"]').length > 0).toString());
+  }
+
+  function openTab($label, $content) {
+    $label
+      .attr({
+        'aria-selected' : 'true',
+        'tabindex' : 0
+      })
+      .removeClass('is-inactive')
+      .addClass('is-active');
+
+    $content
+      .attr({
+        'aria-hidden' : 'false',
+        'tabindex' : 0
+      })
+      .removeClass('is-inactive')
+      .addClass('is-active');
+  }
+
+  function closeTab($label, $content) {
+    $label
+      .attr({
+        'aria-selected' : 'false',
+        'tabindex' : 0
+      })
+      .addClass('is-inactive')
+      .removeClass('is-active');
+
+    $content
+      .attr({
+        'aria-hidden' : 'true',
+        'tabindex' : -1
+      })
+      .addClass('is-inactive')
+      .removeClass('is-active');
+  }
+
+  function closeAllTabs($tabs) {
+
+  }
+
+  setup();
+})(window);
