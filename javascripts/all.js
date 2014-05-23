e60f389ed7718a3e01c8d2024a8184b45fd8b9c4
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-05-23 14:21:16 +0100

    Site updated to 0de859c

--- javascripts/all.js
+++ javascripts/all.js
@@ -370,6 +370,55 @@
 ;(function(window){
   'use strict';
 
+  var $itemInputs = $('.js-item-value');
+  var $itemsTotal = $('.js-item-total');
+  var $itemToggles = $('.js-toggle');
+  var total = 0;
+
+  function setup() {
+    $itemToggles.on('click', function(e) {
+      e.preventDefault();
+      handleItemToggle($(this));
+    });
+
+    $itemInputs.on('keyup', function(e) {
+      handleItemKeyEvent();
+    });
+  }
+
+  function showItem($trigger) {
+    var $target = $('[data-toggle-target=' + $trigger.data('toggle-trigger') + ']');
+    $target.attr({
+      'aria-hidden' : true
+    });
+  }
+
+  function handleItemKeyEvent() {
+    update(calculateTotal());
+  }
+
+  function handleItemToggle($trigger) {
+    showItem($trigger);
+  }
+
+  function calculateTotal() {
+    var itemsTotal = 0;
+    $itemInputs.each(function(i) {
+      itemsTotal += parseFloat($(this).val(),10) || 0;
+    });
+    return itemsTotal;
+  }
+
+  function update(itemsTotal) {
+    total = itemsTotal;
+    $itemsTotal.html("&pound" + itemsTotal.toFixed(2));
+  }
+
+  setup();
+})(window);
+;(function(window){
+  'use strict';
+
   var $component;
   var $componentLabels;
 
