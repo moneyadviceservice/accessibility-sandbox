636d37393d3624f05fc839834a515ea912b1e380
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-05-29 16:42:26 +0100

    Site updated to 5cbada3

--- javascripts/all.js
+++ javascripts/all.js
@@ -442,11 +442,13 @@
       this.convertLinksToButtons();
       this.$currentTab = this.$tabs.first();
       this.$currentTabLink = this.$tabLinks.first();
+
+      this.selectTab(this.$currentTabLink);
     },
 
     handleTabsAction : function(e, $tabLink) {
       e.preventDefault();
-      this.selectTab($tabLink);
+      this.selectTab($tabLink, true);
     },
 
     convertLinksToButtons : function() {
@@ -471,7 +473,7 @@
       });
     },
 
-    selectTab : function($tabLink) {
+    selectTab : function($tabLink, setFocus) {
       // Store previous tab
       this.$previousTabLink = this.$currentTabLink;
       this.$previousTab = this.$currentTab;
@@ -507,8 +509,11 @@
         .addClass(this.activeClass)
         .attr({
           'aria-hidden': 'false'
-        })
-        .focus();
+        });
+
+      if(setFocus){
+        this.$currentTab.focus();
+      }
 
       this.shiftTabIndex();
     },
