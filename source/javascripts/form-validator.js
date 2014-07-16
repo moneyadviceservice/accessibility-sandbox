1231f6b62dfb70da22712f620b3be90886c3a7c8
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-07-16 10:23:49 +0100

    Add validation for checkboxes, radio and multiple selects
    Now also focuses the form on the validation error message on submit.

--- source/javascripts/form-validator.js
+++ source/javascripts/form-validator.js
@@ -6,9 +6,12 @@
 
     this.requiredFieldSelector = options.requiredFieldSelector || '[aria-required]';
     this.inputParentSelector = options.inputParentSelector || '.form__row';
+    this.inputGroupItemSelector = options.inputGroupItemSelector || '.form__group-item';
+    this.errorContainer = options.errorContainer || '.js-error-container';
     this.alertSelector = options.alertSelector || '.js-alert';
     this.requiredFields = [];
     this.summaryIsVisible = false;
+    this.eventType = null;
     this.$form = $(options.formSelector);
     this.$currentField = null;
     this.$errorSummary = $('.js-error-summary');
@@ -45,6 +48,7 @@
 
   FormValidator.prototype.handleFieldBlur = function(e) {
     if(this.validateOnBlur) {
+      this.eventType = 'blur';
       this.setCurrentField($(e.target));
       this.checkValidity(this.$currentField);
       this.updateSummary();
@@ -52,6 +56,7 @@
   };
 
   FormValidator.prototype.handleFormSubmit = function() {
+    this.eventType = 'submit';
     this.checkForm();
     return false;
   };
@@ -114,7 +119,7 @@
 
     this.removeInlineError(this.$currentField);
 
-    this.findParent(this.$currentField)
+    this.findParentContainer(this.$currentField)
       .addClass(this.validClass)
       .removeClass(this.parentInvalidClass);
   };
@@ -130,13 +135,25 @@
 
     this.addInlineError(this.$currentField);
 
-    this.findParent(this.$currentField)
+    this.findParentContainer(this.$currentField)
       .addClass(this.parentInvalidClass)
       .removeClass(this.validClass);
   };
 
-  FormValidator.prototype.findParent = function($field) {
-    return $field.closest(this.inputParentSelector);
+  FormValidator.prototype.findParentContainer = function($field) {
+    var $fieldContainer = $field.closest(this.inputGroupItemSelector),
+        fieldType = this.getFieldType($field);
+
+    if((fieldType === 'select' || fieldType === 'checkbox') && $fieldContainer.length){
+      return $fieldContainer;
+    }
+    else {
+      return $field.closest(this.inputParentSelector);
+    }
+  };
+
+  FormValidator.prototype.findErrorContainer = function($field) {
+    return $field.siblings(this.inputParentSelector);
   };
 
   FormValidator.prototype.setCurrentField = function($field) {
@@ -186,7 +203,7 @@
   };
 
   FormValidator.prototype.isRadioChecked = function($field) {
-    return !!this.findParent($field).find('[name="' + $field.attr('name') + '"]').filter(':checked').length;
+    return !!this.findParentContainer($field).find('[name="' + $field.attr('name') + '"]').filter(':checked').length;
   };
 
   FormValidator.prototype.isValidEmail = function($field) {
@@ -211,16 +228,16 @@
         errorId = 'error-inline-' + $field.attr('id'),
         $error;
 
-    if(!this.findParent($field).find('.js-error-text').length){
-      $error = $('<span/>').addClass('js-error-text').attr('id',errorId).text(errorMsg);
-      this.findParent($field).find('.js-error-container').append($error);
+    if(!this.findParentContainer($field).find('.js-error-text').length){
+      $error = $('<span/>').addClass('js-error-text form__row-error').attr('id',errorId).text(errorMsg);
+      this.findParentContainer($field).find('.js-error-container').append($error);
     }
 
     this.addAlert(this.$alert, errorMsg);
   };
 
   FormValidator.prototype.removeInlineError = function($field) {
-    this.findParent($field).find('.js-error-text').remove();
+    this.findParentContainer($field).find('.js-error-text').remove();
   };
 
   // FormValidator.prototype.addSummaryError = function($field) {
@@ -268,7 +285,12 @@
 
   FormValidator.prototype.updateErrorCountMessage = function() {
     var errorLength = this.getErrorCount();
-    this.addLiveRegion(this.$errorCount, errorLength + (errorLength > 1? ' errors' : ' error') + ' prevented calculation:' );
+    this.addLiveRegion(this.$errorCount, errorLength + (errorLength > 1? ' errors' : ' error') + ' prevented calculation<span class="visually-hidden">. Tab to cycle through errors</span> :' );
+    if(this.eventType === 'submit') {
+      console.log(this.eventType);
+      this.$errorCount.attr('tabindex',-1);
+      this.$errorCount[0].focus();
+    }
   };
 
   FormValidator.prototype.showSummary = function() {
